508ff0df757c264e66d4ffd796f7bcbbe4227cf1
axis <94421127+axisotherwise@users.noreply.github.com>
2022-07-12 15:43:09 +0900

    Delete db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb+srv://thelapssql:581ysg2315@cluster0.efos6wv.mongodb.net/Cluster0?retryWrites=true&w=majority")
-db = client.musical
-
-user = db.users.find_one({ "name": "bob" }, { "_id": False })
-print((user))
