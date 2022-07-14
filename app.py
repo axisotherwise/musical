34d694e4380b34b85ad43d0603f6d4b842210b3f
taesikyoon <90764424+taesikyoon@users.noreply.github.com>
2022-07-14 15:18:25 +0900

    0714 완

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ client = MongoClient(
     'mongodb+srv://taesikyoon97:louis17467@cluster0.ncjxm.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-# main 페이지
-@app.route('/main')
+# main 페이지 완
+@app.route('/main',methods=['POST','GET'])
 def main():
-    musical_list= list(db.musicals.find({}, {'_id': False}).limit(28))
-    return render_template("main.html", musicals=musical_list)
-
+    token_receive = request.cookies.get('token')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print("토큰아이디 "+payload['email'])
+        user = db.users.find_one({"email": payload["email"]})
+        musical_list = list(db.musicals.find({}, {'_id': False}).limit(28))
+        return render_template("main.html", musicals=musical_list, username=payload['id'])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("index", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("index", msg="로그인 정보가 존재하지 않습니다."))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
